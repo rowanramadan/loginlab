73bb1020487809d96e410944dcd5719fa1003f22
rowanramadan <118815233+rowanramadan@users.noreply.github.com>
2022-12-10 00:06:49 +0200

    Add files via upload

---
+++ login.js
@@ -0,0 +1,27 @@
+function validate()
+{
+// var username=document.getElementById("username").value;
+// var password=document.getElementById("password").value;
+// if(username=="admin"&& password=="123")
+// {
+//     alert("welcome!");
+//     return false;
+// } else { 
+//     alert("not registratio"); 
+
+
+
+
+let enter;
+
+if(username=="admin"&& password=="123"){
+ enter = "welcome";
+document.getElementById("demo").innerHTML =enter;
+} else {
+enter = "nooo";
+}
+
+document.getElementById("demo").innerHTML =enter;
+
+
+}
